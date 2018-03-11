3f2804ab70b66074a61d6bec03533bd32636a7f3
Vaibhav Jayaraman <vaibhavj@berkeley.edu>
2018-03-11 14:50:27 -0700

    checking alert

--- historymap/main/static/main/js/map_init.js
+++ historymap/main/static/main/js/map_init.js
@@ -1,14 +1,8 @@
-var map;
-document.addEventListener('DOMContentLoaded', function() {
-	var api_key = 'AIzaSyBByYGtP4MGtNAd9-SxOPdXk5WJML9b1gA'
-    if (document.querySelectorAll('map').length > 0) {
-        var js_file = document.createElement('script');
-        js_file.type = 'text/javascript';
-        js_file.src = 'https://maps.googleapis.com/maps/api/js?key=${api_key}&callback=initMap';
-		document.getElementsByTagName('head')[0].appendChild(js_file);
-    }
-}
+alert("qwerty");
 
+
+var map;
+$("map").append("Append test");
 function initMap() {
 	var latitude = 59.925580
 	var longitude = 30.295948
@@ -17,8 +11,15 @@ function initMap() {
 		zoom: 8 
 	});
 }
-
 $(document).ready(function() {
+	var api_key = 'AIzaSyBByYGtP4MGtNAd9-SxOPdXk5WJML9b1gA'
+    var js_file = document.createElement('script');
+    js_file.type = 'text/javascript';
+    js_file.src = 'https://maps.googleapis.com/maps/api/js?key=api_key&callback=initMap';
+    $("map").append(js_file);
+}
+
+$(window).load(function() {
     /**Updates map with article markers if mouse has moved**/
     function calculateCoordinates(pnt) {
         /**Returns information about point **/
@@ -40,7 +41,7 @@ $(document).ready(function() {
         var file_return_limit = 50;
         $.ajax({
             type: 'POST',
-            url: "api.php?action=query&list=geosearch&gscoord=${lat}|${lng}&gsradius=${radius}&gslimit=${file_return_limit}",
+            url: "https://en.wikipedia.org/w/api.php?action=query&list=geosearch&gscoord=${lat}|${lng}&gsradius=${radius}&gslimit=${file_return_limit}",
             data: {lat: map_state.lat, lng: map_state.lng, zoom: map.zoom},
             dataType: "json";
             context: document.body,
