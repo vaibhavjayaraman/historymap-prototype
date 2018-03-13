5762af35baa3c969b61ac1e33cf95bdef5777fe9
Vaibhav Jayaraman <vaibhavj@berkeley.edu>
2018-03-12 17:13:00 -0700

    fixed css link

--- historymap/main/static/main/js/map_init.js
+++ historymap/main/static/main/js/map_init.js
@@ -1,64 +1,23 @@
 var map;
+var latitude;
+var longitude;
+
 function initMap() {
-	var latitude = 59.925580
-	var longitude = 30.295948
-	map = new google.maps.Map(document.getElementById('#map'), {
+	latitude = 59.925580;
+	longitude = 30.295948;
+	map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: latitude, lng: longitude},
-		zoom: 8 
+		zoom: 6 
 	});
 }
-$(document).ready(function() {
-	var api_key = 'AIzaSyBByYGtP4MGtNAd9-SxOPdXk5WJML9b1gA';
+
+function map_call() {
+    var api_key =  'AIzaSyAa8xh6DKU5tSCrqMc22HeLdXHDWoRioRw';
+    var url = 'https://maps.googleapis.com/maps/api/js?key=' + api_key + '&callback=initMap';
     var js_file = document.createElement('script');
     js_file.type = 'text/javascript';
-    js_file.src = 'https://maps.googleapis.com/maps/api/js?key=api_key&callback=initMap';
+    js_file.src = url;
     $("#map").append(js_file);
 }
 
-function calculateCoordinates(pnt) {
-    /**Returns information about point **/
-    var obj = {
-        lat: pnt.lat().toFixed(4),
-        lng = pnt.lng().toFixed(4),
-        zoom: map.zoom,
-    }
-    return obj;
-}
-
-$(window).load(function() {
-    /**Updates map with article markers if mouse has moved**/
-    google.maps.event.addListener(map, 'click', function (event) {
-        /**checks to make sure map.zoom is bigger than 16 */
-        if (map.zoom > 16) {
-        var map_state = calculateCoordinates(event.latlng);
-        var lat = map_state.lat;
-        var lng = map_state.lng;
-        var radius = 6;
-        var file_return_limit = 50;
-        $.ajax({
-            type: 'POST',
-            url: "https://en.wikipedia.org/w/api.php?action=query&list=geosearch&gscoord=${lat}|${lng}&gsradius=${radius}&gslimit=${file_return_limit}",
-            data: {lat: map_state.lat, lng: map_state.lng, zoom: map.zoom},
-            dataType: "json";
-            context: document.body,
-            global: false,
-            async: false,
-            success: function(data) {
-                function plotMarkers(markers) {
-                    markers.forEach(function (marker) {
-                        var position = new google.maps.LatLng(
-                            marker.lat, 
-                            marker.lng);
-                        var marker = new google.maps.Marker({
-                            position: position,
-                            map: map,
-                            animation: google.maps.Animation.DROP
-                        });
-                    });
-                }
-            }
-        });
-        }
-    });
-});
-
+map_call();
