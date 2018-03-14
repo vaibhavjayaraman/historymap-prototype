ba2bed4ede21322deb650b063d4f38b733260f6c
Vaibhav Jayaraman <vaibhavj@berkeley.edu>
2018-03-14 03:42:38 -0700

    changed js to allow for CORS

--- historymap/main/static/main/js/map_init.js
+++ historymap/main/static/main/js/map_init.js
@@ -7,7 +7,7 @@ function initMap() {
 	longitude = 30.295948;
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: latitude, lng: longitude},
-		zoom: 6 
+		zoom: 17 
 	});
 }
 
@@ -21,51 +21,47 @@ function map_call() {
 }
 
 map_call();
-function calculateCoordinates(pnt) {
-    /**Returns information about point **/
-    var obj = {
-        lat: pnt.lat().toFixed(4),
-        lng: pnt.lng().toFixed(4),
-        zoom: map.zoom,
-    }
-    return obj;
-}
 
-$(window).load(function() {
-    /**Updates map with article markers if mouse has moved**/
+/**Updates map with article markers if mouse has moved**/
 
-    google.maps.event.addListener(map, 'click', function (event) {
+$(document).ready(function() { 
+    $(window).on('load', function() {
+        google.maps.event.addListener(map, 'click', function (event) {
         /**checks to make sure map.zoom is bigger than 16 */
-        if (map.zoom > 16) {
-            var map_state = calculateCoordinates(event.latlng);
-            var lat = map_state.lat;
-            var lng = map_state.lng;
-            var radius = 6;
-            var file_return_limit = 50;
-            console.log(lat, lng, radius);
-            $.ajax({
-                type: 'POST',
-                url: "https://en.wikipedia.org/w/api.php?action=query&list=geosearch&gscoord=${lat}|${lng}&gsradius=${radius}&gslimit=${file_return_limit}",
-                data: {lat: lat, lng: lng, zoom: zoom},
-                dataType: "json",
-                context: document.body,
-                global: false,
-                async: false,
-                success: function(data) {
-                    function plotMarkers(markers) {
-                        markers.forEach(function (marker) {
-                            var position = new google.maps.LatLng(
-                                marker.lat, 
-                                marker.lng);
-                            var marker = new google.maps.Marker({
-                                position: position,
+            var zoom = map.getZoom();
+            if (zoom > 16) {
+                var pos = event.latLng;
+                var lat = pos.lat();
+                var lng = pos.lng();
+                var radius = 1000;
+                var file_return_limit = 10; 
+                $.ajax({
+                    type: 'POST',
+                    url: "https://en.wikipedia.org/w/api.php?origin=*&action=query&list=geosearch&gscoord="+ lat +"|" + lng +"&gsradius=" + radius +"&gslimit=" + file_return_limit + "&format=json",
+                    data: {lat: lat, lng: lng, zoom: zoom},
+                    dataType: "json",
+                    context: document.body,
+                    global: false,
+                    async: true,
+                    crossDomain:true,
+                    success: function(data) {
+                        alert(url);
+                        alert(data);
+                        alert(Object.keys(data));
+                        alert(Object.values(data));
+                        var parsedData = JSON.parse(data);
+                        var articles = parsedData.query.geosearch;
+                        for (var i = 0; i < articles.length; i++) {
+                            var article = articles[i];
+                            new google.maps.Marker({
+                                position: {lat: article.lat, lng: article.lng},
                                 map: map,
-                                animation: google.maps.Animation.DROP
+                                title: article.title,
                             });
-                        });
+                        }
                     }
-                }
-        });
-        }
-    });
+                });
+            }
+        })
+    })
 });
