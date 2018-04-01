6398d21a0875dfb14e844766afaf7bf0eba5b4ec
Vaibhav Jayaraman <vaibhavj@berkeley.edu>
2018-04-01 02:31:13 -0700

    switched from google maps to leaflet

--- historymap/main/static/main/js/map_init.js
+++ historymap/main/static/main/js/map_init.js
@@ -1,38 +1,68 @@
 var map;
-var latitude;
+var latitude; 
 var longitude;
+var zoom;
 
+/**Initializes map. **/
 function initMap() {
-	latitude = 59.925580;
-	longitude = 30.295948;
-	map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: latitude, lng: longitude},
-		zoom: 17 
-	});
+	latitude = 59.925580; //will be changed to use users last session data
+	longitude = 30.295948; // will be changed to use users last session data
+    zoom = 16; 
+	map = L.map('map').setView([latitude, longitude], zoom);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', 
+	{
+		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+		maxZoom: 18,
+		id: 'mapbox.streets',
+		accessToken: 'pk.eyJ1IjoidmFpYmhhdmoiLCJhIjoiY2pmZ2d1NDVjMjdzMDMzbWlhdTRtZXAyZyJ9.X3KDHMveDXHRh795LdSFmw',
+    }).addTo(map);
 }
 
-function map_call() {
-    var api_key =  'AIzaSyAa8xh6DKU5tSCrqMc22HeLdXHDWoRioRw';
-    var url = 'https://maps.googleapis.com/maps/api/js?key=' + api_key + '&callback=initMap';
-    var js_file = document.createElement('script');
-    js_file.type = 'text/javascript';
-    js_file.src = url;
-    $("#map").append(js_file);
+/** Loads and places wikipedia articles on map. **/
+function wiki_call(url) {
+	alert(url);
+    $.ajax({
+        type: 'post',
+        url: url,
+        datatype: "json",
+        context: document.body,
+        global: false,
+        async: true,
+        crossdomain:true,
+        success: function(data) {
+            var parsed_data = data
+            var articles = parsed_data.query.geosearch;
+            for (var i = 0; i < articles.length; i++) {
+                var article = articles[i];
+                var lat = article.lat;
+                var lon = article.lon;
+				var title = article.title;
+                var marker = L.marker([lat,lon]);
+                marker.addTo(map);
+                var url = "https://en.wikipedia.org/?curid="
+                            + article.pageid;
+				var marker_popup = function(marker, url, title) {
+					return function() {
+						marker.bindPopup(title.bold() + "\n" + url).openPopup();
+					}
+				};
+                marker.on('mouseover', marker_popup(marker, url, title)); 
+            }
+        }
+    });
 }
 
-map_call();
+initMap();
 
-/**Updates map with article markers if mouse has moved**/
-
+/**Updates map with article markers if mouse has moved. **/
 $(document).ready(function() { 
     $(window).on('load', function() {
-        google.maps.event.addListener(map, 'click', function (event) {
+        map.on('click', function(e) {
         /**checks to make sure map.zoom is bigger than 16 */
             var zoom = map.getZoom();
             if (zoom > 16) {
-                var pos = event.latLng;
-                var lat = pos.lat();
-                var lng = pos.lng();
+                var lat = e.latlng.lat;
+                var lng = e.latlng.lng;
                 var radius = 1000;
                 var file_return_limit = 10; 
                 url = "https://en.wikipedia.org/w/api.php?" + 
@@ -40,37 +70,9 @@ $(document).ready(function() {
                     "&gsradius=" + radius + "&gslimit=" + file_return_limit +
                     "&prop=info|extracts&inprop=url" +
                     "&format=json";
-                $.ajax({
-                    type: 'POST',
-                    url: url,
-                    dataType: "json",
-                    context: document.body,
-                    global: false,
-                    async: true,
-                    crossDomain:true,
-                    success: function(data) {
-                        var parsed_data = data
-                        var articles = parsed_data.query.geosearch;
-                        for (var i = 0; i < articles.length; i++) {
-                            var article = articles[i];
-                            marker = new google.maps.Marker({
-                                position: {lat: article.lat, lng: article.lon},
-                                map: map,
-                                title: article.title,
-                            });
-                            var infowindow = new google.maps.InfoWindow();
-                            marker.content =  "https://en.wikipedia.org/?curid="
-                                        + article.pageid;
-                            google.maps.event.addListener(marker, "mouseover", (function(marker) {
-                                return function() {
-                                    infowindow.setContent(this.content);
-                                    infowindow.open(map, this);
-                                }
-                            })(marker));
-                        }
-                    }
-                });
+                wiki_call(url);
             }
         });
     });
 });
+
