e76f400816b314d1b6f2cb225234707d73f7ae3b
Vaibhav Jayaraman <vaibhavj@berkeley.edu>
2018-05-22 18:26:57 -0700

    added geotiff sampleimage

--- tile_processing/create_region.py
+++ tile_processing/create_region.py
@@ -6,6 +6,8 @@ EDGE = "/edge/"
 TEXT = "/text/"
 NAMES = "/names/"
 EDGE_ON_TEXT = "/edge_on_text/"
+MASK = "/mask/"
+GEOTIFF = "/geotiff/"
 
 def create_region(region):
     """Creates directory  and subdirectories for maps of region. """
@@ -13,6 +15,9 @@ def create_region(region):
     os.makedirs(TILE_PICTURE_LOCATIONS + region + ORIGINAL)
     os.makedirs(TILE_PICTURE_LOCATIONS + region + EDGE)
     os.makedirs(TILE_PICTURE_LOCATIONS + region + TEXT)
+    os.makedires(TILE_PICTURE_LOCATIONS + region + MASK)
+    os.makedires(TILE_PICTURE_LOCATIONS + region + GEOTIFF)
+
 
 def move_file(path, region, year):
     """Moves files to correct region. """
