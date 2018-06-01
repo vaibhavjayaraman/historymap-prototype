d97f3a50ca1c25bdd20e1d8baae7be73d85d8be3
Vaibhav Jayaraman <vaibhavj@berkeley.edu>
2018-06-01 13:33:18 -0700

    changed tile_deploy.py to symlink png instead of directory holding png(s)

--- tile_deploy.py
+++ tile_deploy.py
@@ -1,6 +1,6 @@
 from os import rename, symlink, makedirs, listdir, getcwd
 from os.path import isdir, islink 
-"""Adds tiles from region to tile server. Modularize and allow for one year to be done at a time. """
+"""Adds tiles from region to tile server. Modularize and allow for one year to be done at a time."""
 def move_tiles(region, path = "tile_processing/test_pictures/", tile_server_base = "tile_server/", zoom = 8):
     base = path + region + "/" + "tiles/"
     years = [_dir for _dir in listdir(base) if isdir(base + _dir)]
@@ -15,12 +15,22 @@ def move_tiles(region, path = "tile_processing/test_pictures/", tile_server_base
         for _zoom in zoom_levels:
             try:
                 if int(_zoom) <= zoom:
-                    for _file in listdir(year_base + _zoom):
-                        _file_path = year_base + _zoom + "/" + _file
-                        _ts_file = tile_server_base + year + "/" + _zoom + "/" + _file
-                        if not islink(_file_path):
-                            rename(_file_path, _ts_file)
-                            symlink(getcwd()+ "/" + _ts_file, _file_path)
+                    for column in listdir(year_base + _zoom):
+                        # column_dir is directory in tile_server
+                        # column_path is location of column directory in region tile directory
+                        column_dir = tile_server_base + year + "/" + _zoom + "/" + column
+                        column_path = year_base + _zoom + "/" + column
+                        if not isdir(column_dir):
+                            makedirs(column_dir)
+                        for _png in listdir(column_path):
+                            region_file_path = column_path+ "/" + _png
+                            ts_file_path = column_dir + "/" + _png
+                            if not islink(region_file_path):
+                                try:
+                                    rename(region_file_path, ts_file_path)
+                                    symlink(getcwd()+ "/" + ts_file_path, region_file_path)
+                                except OSError:
+                                    print("Column: " + column + ",_png: " + _png + " already exists in tile_server") 
             except ValueError as e: 
                 print(_zoom + "cannot be converted to int") 
 
