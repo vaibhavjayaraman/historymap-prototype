e4cd751c5e9db89468a5d872c0f2df4deb0185a3
Vaibhav Jayaraman <vaibhavj@berkeley.edu>
2018-07-10 03:15:32 -0700

    got rid of masking sparse areas of map - this may destroy info for other time periods that are not settled in mask year

--- tile_processing/mask_tiles.py
+++ tile_processing/mask_tiles.py
@@ -19,9 +19,6 @@ def create_masked_png(region, filename, sparse, mask = "mask.png"):
     #change all bits that are 0 to 1 here so that they do not become transparent later
     img[np.where((img == [0,0,0]).all(axis = 2))] = [1,1,1]
     msk = cv2.imread(TILE_PICTURE_LOCATIONS + region + MASK + region + mask, 0) 
-    #if image is sparse then get rid of white spots (this will create complictation with text that is white though)
-    if sparse:
-        img[np.where((img == [255,255,255]).all(axis = 2))] = [0,0,0]
     masked = cv2.bitwise_and(img, img, mask = msk)
     cv2.imwrite(TILE_PICTURE_LOCATIONS + region + MASKED + filename, masked)
 
@@ -35,7 +32,7 @@ def make_transparent_png(region, source = MASKED, dest = TRANSPARENT_PNG):
         system(convert)
     print("Made pngs transparent from " + source + " to " + dest)
 
-def mask_images(region, sparse = False):
+def mask_images(region):
     for _file in tqdm(listdir(TILE_PICTURE_LOCATIONS + region + ORIGINAL)):
         create_masked_png(region, _file, sparse)
     print("masked all PNGS in region")
